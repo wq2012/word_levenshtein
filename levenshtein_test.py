53782d6ed6e2d3cc68d6375d59dd78d488410a36
Quan Wang <quanw@google.com>
2024-09-23 13:53:25 -0400

    publish script

--- levenshtein_test.py
+++ levenshtein_test.py
@@ -4,8 +4,9 @@ import word_levenshtein
 
 class LevenshteinTest(unittest.TestCase):
 
-  def test_a(self):
-    pass
+  def test_str_split(self):
+    splitted = word_levenshtein.str_split("a b c", " ")
+    self.assertEqual(splitted, ["a", "b", "c"])
 
 if __name__ == "__main__":
   unittest.main()
